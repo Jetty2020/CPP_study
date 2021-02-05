42d4949c06a012fc15c4eb6257f404a99201dfdb
Jetty2020 <kmclo@naver.com>
2021-02-05 20:59:38 +0900

    Chapter.8

---
+++ folder/8_6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class IntArray
+{
+private:
+    int *m_arr = nullptr;
+    int m_length = 0;
+
+public:
+    IntArray(const int& length_in)
+    {
+        m_length = length_in;
+        m_arr = new int[m_length];
+
+        cout << "Constructor " << endl;
+    }
+
+    ~IntArray()
+    {
+        if(m_arr != nullptr)
+            delete[] m_arr;
+        cout << "Desrtuctor " << endl;
+    }
+    int size() { return m_length; }
+};
+
+int main()
+{
+    while (true)
+        IntArray my_int_arr(10000);
+
+    return 0;
+}
